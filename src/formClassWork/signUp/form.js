d5d829ec691af60cd5a10ffcaa79ad8778338cb2
Uyai20 <etukuyaiabasi20@gmail.com>
2022-09-01 13:20:46 -0700

    added autoFocus

--- src/formClassWork/signUp/form.js
+++ src/formClassWork/signUp/form.js
@@ -9,15 +9,16 @@ const Form = () => {
       <div className='general'>
         <form action="">
           <h1>Sign Up</h1>
-          <input placeholder='Username' type="text" className="user-input"/>
-          <input placeholder='Your Email' type="email" className="user-input"/>
-          <input placeholder='Password' type="password" className="user-input"/>
-          <input placeholder='Repeat Your Password' type="password" className="user-input"/>
-          <label className="user-input"><input type='checkbox'/>I agree all statements in <a href='/'>Terms Of Service</a></label>
-          <button>Register</button>
+          <input autoFocus placeholder='Username' type="text" className="user-input" required/>
+          <input placeholder='Your Email' type="email" className="user-input" required/>
+          <input placeholder='Password' type="password" className="user-input" required/>
+          <input placeholder='Repeat Your Password' type="password" className="user-input" required/>
+          <label className="user-input"><input type='checkbox' required/>I agree all statements in <a href='/'>Terms Of Service</a></label>
+          <button type='submit'>Register</button>
         </form>
         <div>
            <img src={signupImage} alt='design'/>
+           <a href='/'><p>I am already a member</p></a>
         </div>
       </div>
     </div>
